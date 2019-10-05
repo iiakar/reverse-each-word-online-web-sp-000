062fb69c4fd4197757d5d523dfae8214d9121c96
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-05 18:59:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(names)
-  nnames = names.split("")
+  
   newarray = Array.new
-  nnames.collect {|name| newarray << name.reverse}
+  names.collect {|name| newarray << name.reverse}
+  puts newarray
 end 
