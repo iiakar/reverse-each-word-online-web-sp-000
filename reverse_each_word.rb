250bc9f8b5356114de538525a2fb2f1e9167a8d2
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-05 19:10:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string = string.split("")
   reversed = []
-  string.collect {|i| reversed.reverse!}
+  string.collect {|i| reversed << i.reverse!}
   return reversed
 end 
 
